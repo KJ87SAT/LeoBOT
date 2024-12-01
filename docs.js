e1ccf3b70fface596029c0a2a20d20605b457bca
KJ87SAT <kenjikubo0324@gmail.com>
2024-12-01 19:13:11 +0900

    Update docs.js

--- docs.js
+++ docs.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     // コマンドデータ
     const commands = [
-        { category: 'admin', title: 'afk', description: 'ユーザーをAFK状態にします。', usage: '/afk [reason]' },
-        { category: 'admin', title: 'back', description: 'ユーザーのAFK状態を解除します。', usage: '/back' },
-        { category: 'admin', title: 'create_role_panel', description: 'ロールパネルを作成します。', usage: '/create_role_panel [role]' },
-        { category: 'admin', title: 'daychat', description: '一日限定のチャットを初期化します。', usage: '/daychat' },
-        { category: 'admin', title: 'delete_verifier', description: '認証メッセージを削除します。', usage: '/delete_verifier' },
-        { category: 'admin', title: 'enddaychat', description: '1Dayチャットを手動で終了します。', usage: '/enddaychat' }
+        { category: '1', title: 'afk', description: 'ユーザーをAFK状態にします。', usage: '/afk [reason]' },
+        { category: '1', title: 'back', description: 'ユーザーのAFK状態を解除します。', usage: '/back' },
+        { category: '1', title: 'create_role_panel', description: 'ロールパネルを作成します。', usage: '/create_role_panel [role]' },
+        { category: '1', title: 'daychat', description: '一日限定のチャットを初期化します。', usage: '/daychat' },
+        { category: '1', title: 'delete_verifier', description: '認証メッセージを削除します。', usage: '/delete_verifier' },
+        { category: '1', title: 'enddaychat', description: '1Dayチャットを手動で終了します。', usage: '/enddaychat' }
     ];
 
     const commandList = document.getElementById('command-list');
